7d21e1e05115f633cbf5790bac373e5a64b9066e
x4iiiis <ryan_o_f@hotmail.co.uk>
2019-04-22 15:18:52 +0100

    Started with php artisan make:auth
    some pictures of Mo on the main page too - meant to be a carousel but it's broken for some reason. More bothered about functionality than prettiness at the moment however. Planning to use Postman as I build a RESTFUL API

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,6 +94,35 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
-        </div>
+            </div>
+
+            <div class="container">
+
+            <div id="carouselExampleControls" class="carousel slide shadow-lg border border-dark rounded" data-ride="carousel">
+           <div class="carousel-inner">
+             <div class="carousel-item active">
+               <img class="d-block w-100" src="\img\4.jpg" alt="First slide">
+             </div>
+             <div class="carousel-item">
+               <img class="d-block w-100" src="\img\1.jpg" alt="Second slide">
+             </div>
+             <div class="carousel-item">
+               <img class="d-block w-100" src="\img\3.jpg" alt="Third slide">
+            </div>
+           </div>
+           <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+             <span class="sr-only">Previous</span>
+           </a>
+           <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+             <span class="carousel-control-next-icon" aria-hidden="true"></span>
+             <span class="sr-only">Next</span>
+           </a>
+         </div>
+     </div>
+
+     </div>
+
+
     </body>
 </html>
