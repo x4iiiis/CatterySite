98d9d6ec507bddae47a70ce17c2c30fd318a5aee
x4iiiis <ryan_o_f@hotmail.co.uk>
2019-04-22 15:55:22 +0100

    Setup for backend of Booking work
    A bit of Vue.js coming up next - worth noting that whatever I do next is probably just experimental and/or temporary as the actual booking system will most likely be a complex front-to-back-end component

---
+++ database/migrations/2019_04_22_142355_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('inDateTime');
+            $table->dateTime('outDateTime');
+            $table->string('petSpecies', 100);
+            $table->string('petName', 100);
+            $table->string('specialInstructions');
+            $table->decimal('userID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
