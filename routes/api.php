1ef93194388b62bb8e5f57ec176953b5dcf3a8b0
x4iiiis <ryan_o_f@hotmail.co.uk>
2019-04-30 14:07:07 +0100

    JWT in place
    Thanks to https://www.youtube.com/watch?v=l201RSQ7Ti4 and https://jwt-auth.readthedocs.io/en/docs/quick-start/

--- routes/api.php
+++ routes/api.php
@@ -2,19 +2,17 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group([
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
 });
 
 Route::post('/booking', 'BookingController@create')->name('createbooking');
